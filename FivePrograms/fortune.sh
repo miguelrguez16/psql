42cffaad6f36a91e4e2c3cee6afff4d3b7d0def7
Miguel Rguez <uo263494@uniovi.es>
2022-04-08 13:40:21 +0200

    learning env and operators

---
+++ FivePrograms/fortune.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Program to tell a persons fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6))
+echo ${RESPONSES[$N]}
+
+GET_FORTUNE(){
+  echo Ask a yes or no question:
+}
+GET_FORTUNE
