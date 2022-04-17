100561aec78eae6ddb40d503701b7535a93cdf1f
Miguel Rguez <uo263494@uniovi.es>
2022-04-17 23:16:27 +0200

    end of students part2

--- 2_studentDatabase/pruebas.sh
+++ 2_studentDatabase/pruebas.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
-echo -e "\nList of majors, in alphabetical order, that either no student is taking or has a student whose first name contains a case insensitive 'ma':"
-echo "$($PSQL "SELECT majors.major_id, majors.major, students.first_name FROM majors RIGHT JOIN students on students.major_id=majors.major_id GROUP BY majors.major_id, students.first_name HAVING count(majors.major_id)=0 OR students.first_name ILIKE '%ma%'")"
+
+echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
+echo "$($PSQL "SELECT * FROM students FULL JOIN majors USING(major_id) FULL JOIN majors_courses USING(major_id) FULL JOIN courses USING(course_id) GROUP BY majors_courses.course_id, courses.course_id HAVING count(students.student_id)=0 OR students.first_name like 'Obie' ORDER BY course DESC")"
 
 
-echo "SELECT major FROM students RIGHT JOIN majors ON majors.major_id=students.major_id WHERE student_id is null;"
+ # distinct(course), students.first_name
