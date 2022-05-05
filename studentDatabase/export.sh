df4bef313ad0296b18fe8ec715ff382d46125b28
Miguel Rguez <uo263494@uniovi.es>
2022-05-05 16:18:40 +0200

    deleted files

--- studentDatabase/export.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#exportar BBDD de students
-pg_dump -cC --inserts -U freecodecamp students > students.sql
-echo $?
-
-#   50  pg_dump --clean --create --inserts --username=freecodecamp students > students.sql
-
